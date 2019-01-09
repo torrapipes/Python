67a43f4122c33ed90d54bed018aa33a1a98fa22b
torrapipes <crbergas@gmail.com>
2019-01-09 12:51:17 +0100

    created test cases

--- Projects/gildedRose/item.py
+++ Projects/gildedRose/item.py
@@ -1,4 +1,10 @@
+if __name__ == '__main__':
 
+    Duck = Item("Duck", 100, 1)
+
+    assert duck.getName() == "Duck"
+    assert duck.getSell_in() == 100
+    assert duck.getQuality() == 1
     
 
 
