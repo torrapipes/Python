cb695a415382cbedd3d9f1ec270a7b93108a6222
torrapipes <crbergas@gmail.com>
2019-01-09 12:46:28 +0100

    created item class

---
+++ Projects/gildedRose/item.py
@@ -0,0 +1,28 @@
+class Item(self):
+
+
+    def __init__(self, name, sell_in, quality):
+
+        self.name = name
+        self.sell_in = sell_in
+        self.quality = quality
+
+    def __repr__(self, name, sell_in, quality):
+
+        return %s %s %s %(self.getName(), self.getSell_in, self.getQuality)
+
+    def getName(self):
+
+        return self.name
+
+    def getSell_in(self):
+
+        return self.sell_in
+    
+    def getQuality(self):
+
+        return self.quality
+    
+
+
+    
