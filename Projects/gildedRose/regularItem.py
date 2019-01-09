c48dab02b84edb7331301bcf230f73e746926d36
torrapipes <crbergas@gmail.com>
2019-01-09 13:21:05 +0100

    created class regularItem

--- Projects/gildedRose/regularItem.py
+++ Projects/gildedRose/regularItem.py
@@ -1,3 +1,19 @@
+from item import Item
+from updatable import Updatable
+
+class RegularItem(Item, Updatable):
+
+
+    def setSell_in(self):
+        return self.sell_in - 1
+
+    def update_quality(self):
+        if self.sell_in > 0:
+            return self.quality - 1
+        else:
+            return self.quality - 2
+
+
 if __name__ == '__main__':
 
     # CREATED AN OBJECT
